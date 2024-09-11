77446768ac2ac9981d6ddd8ba07675ed15087c13
Vigonair <vighneshnair093@gmail.com>
2024-09-11 23:25:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const toggleButton = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleButton.addEventListener('click', () ={">"} {
+        navLinks.classList.toggle('active')
+    });
+</script>
